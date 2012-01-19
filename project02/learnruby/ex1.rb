713e6dbeebcb73e39983912482e4c3fa80b30c0d
Richie <zirbsster@gmail.com>
2012-01-19 15:49:06 -0700

    attempting to fix project02 problem

--- project02/learnruby/ex1.rb
+++
@@ -1,9 +0,0 @@
-# Ruby the hard way
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts 'Yay! Printing.'
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
-
