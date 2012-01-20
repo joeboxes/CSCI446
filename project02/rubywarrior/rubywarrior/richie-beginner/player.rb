3997819f0e3d461c8dc40a17f6c9c5473b4ae531
Richie <zirbsster@gmail.com>
2012-01-19 20:47:56 -0700

    level 8 on rubywarrior

--- project02/rubywarrior/rubywarrior/richie-beginner/player.rb
+++ project02/rubywarrior/rubywarrior/richie-beginner/player.rb
@@ -1,34 +1,54 @@
 # yippie kay yay
 class Player
+	@@healthMax = 20
 	@@health = 21
+	@@dir = :backward
+	@@hiding = false
 	def play_turn(warrior)
-#		puts "HEALTH: '#{@@health} <= #{warrior.health}"
-		#puts "FEEL: '#{ (warrior.feel).kind_of? Captive }' "
-		@wtf = (warrior.feel)
-#		puts @wtf
-#		puts @wtf.to_s
-#		puts @wtf.kind_of?(Captive)
-#		puts @wtf.is_a?(Captive)
-#		puts @wtf.type_of? Captive
+		@wtf = warrior.feel @@dir
 		@isACaptive = @wtf.to_s=="Captive"
-		puts @wtf.class
-		if @isACaptive
-			warrior.rescue!
-		else
-			if !(@@health>warrior.health) && warrior.health<20 && warrior.feel.empty?
+		@isANothing = @wtf.to_s=="nothing"
+		@isAWall = @wtf.to_s=="wall"
+		#puts @wtf.to_s
+		#puts @wtf.class
+		#puts @isAWall
+		if @isAWall
+			if warrior.health<20
 				warrior.rest!
 			else
-				if !warrior.feel.empty?
-					warrior.attack!
+				flipDirection
+				warrior.walk! @@dir
+				@@hiding = false
+			end
+		elsif @isACaptive
+			warrior.rescue! @@dir
+		elsif @@hiding
+			warrior.walk! @@dir
+		else
+			if warrior.health<6 || (!(@@health>warrior.health) && warrior.health<20 && warrior.feel.empty?)
+				@@hiding = true
+				flipDirection
+				warrior.walk! @@dir
+			else
+				if !(warrior.feel @@dir).empty?
+					warrior.attack! @@dir
 				else
-					warrior.walk!
+					warrior.walk! @@dir
 				end
 			end
 			@@health = warrior.health
 		end
 		puts "HEALTH #{@@health}"
 	end
+	def flipDirection
+		if @@dir==:forward
+			@@dir = :backward
+		else
+			@@dir = :forward
+		end
+	end
 end
+# .pivot
 # .rest
 # .health
 # .rest!
@@ -39,4 +59,11 @@ end
 # .rescue!
 # 
 # 
+# warrior.feel(:backward).wall?.
 # 
+# 
+#		puts @wtf
+#		puts @wtf.to_s
+#		puts @wtf.kind_of?(Captive)
+#		puts @wtf.is_a?(Captive)
+#		puts @wtf.type_of? Captive
