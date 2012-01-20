265791369538bdcbd47ee94f693654e2e23a6b0a
Richie <zirbsster@gmail.com>
2012-01-19 20:16:32 -0700

    played more rubywarrior

--- project02/rubywarrior/rubywarrior/richie-beginner/player.rb
+++ project02/rubywarrior/rubywarrior/richie-beginner/player.rb
@@ -1,5 +1,42 @@
+# yippie kay yay
 class Player
-  def play_turn(warrior)
-    # add your code here
-  end
+	@@health = 21
+	def play_turn(warrior)
+#		puts "HEALTH: '#{@@health} <= #{warrior.health}"
+		#puts "FEEL: '#{ (warrior.feel).kind_of? Captive }' "
+		@wtf = (warrior.feel)
+#		puts @wtf
+#		puts @wtf.to_s
+#		puts @wtf.kind_of?(Captive)
+#		puts @wtf.is_a?(Captive)
+#		puts @wtf.type_of? Captive
+		@isACaptive = @wtf.to_s=="Captive"
+		puts @wtf.class
+		if @isACaptive
+			warrior.rescue!
+		else
+			if !(@@health>warrior.health) && warrior.health<20 && warrior.feel.empty?
+				warrior.rest!
+			else
+				if !warrior.feel.empty?
+					warrior.attack!
+				else
+					warrior.walk!
+				end
+			end
+			@@health = warrior.health
+		end
+		puts "HEALTH #{@@health}"
+	end
 end
+# .rest
+# .health
+# .rest!
+# .feel
+# .feel.empty?
+# .attack!
+# .walk!
+# .rescue!
+# 
+# 
+# 
