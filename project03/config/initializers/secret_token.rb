7019b0ce2a11a872836b293caf75955b90b76903
Richie <zirbsster@gmail.com>
2012-01-26 03:22:38 -0700

    added new project03 rails core

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project03::Application.config.secret_token = 'f5404885bdde7aea902cf3613203a43cfa9c49a773ce9b9f088937e0cfdeea251dcc2575f2b8c8228c68fa3b913eca2c448bea18d3aa2dae8db23bf79eecfb65'
