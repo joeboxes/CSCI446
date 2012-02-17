474e199eb11012831f3fa261b965939623ae5706
Richie <zirbsster@gmail.com>
2012-02-16 21:20:06 -0700

    Finally adding Project04 files...

---
+++ README.txt
@@ -0,0 +1,1 @@
+This folder contains the unit 4 project - Articles
---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = 'f9bc9bb00ffa253813aef23b79a41b908b43fa5a3ce294d5bc0aaa40c9187c4395802d6240b2ba1a24c393fed63806579bc538915f14b7d5ed869b6375bc7ff7'
