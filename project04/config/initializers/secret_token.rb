91da3bc486d410fe97f47cdf998ed61213f20dac
Richie <zirbsster@gmail.com>
2012-02-18 16:44:37 -0700

    added base files to project04

--- project04/config/initializers/secret_token.rb
+++ project04/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Articles::Application.config.secret_token = 'f9bc9bb00ffa253813aef23b79a41b908b43fa5a3ce294d5bc0aaa40c9187c4395802d6240b2ba1a24c393fed63806579bc538915f14b7d5ed869b6375bc7ff7'
+Articles::Application.config.secret_token = '2a367c0b93e0ff9ed0dbbc91567e91b1c4bd9efa24019e0fb92b55a0575def694bc05d9984b53e9ce6e2a56e2e13d810970627529c21672ba2438384ae58de7e'
