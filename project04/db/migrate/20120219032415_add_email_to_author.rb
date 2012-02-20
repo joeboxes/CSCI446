7296b9e0ab48d19c3b394f608b62e1a3548c2618
Richie <zirbsster@gmail.com>
2012-02-19 17:59:17 -0700

    yo

--- project04/db/migrate/20120219032415_add_email_to_author.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailToAuthor < ActiveRecord::Migration
-  def change
-    add_column :authors, :email, :string
-  end
-end
--- project04/db/migrate/20120219040431_remove_created_from_article.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCreatedFromArticle < ActiveRecord::Migration
-  def up
-    remove_column :articles, :created
-  end
-
-  def down
-    add_column :articles, :created, :datetime
-  end
-end
