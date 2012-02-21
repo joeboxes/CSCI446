37bca3741c2221f95e21a45f1282e5f67c37e58c
Richie <zirbsster@gmail.com>
2012-02-20 22:28:42 -0700

    added restriction to username Pat

--- project04/app/models/author.rb
+++ project04/app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-	validates :username, presence: true, uniqueness: true
+	validates :username, presence: true, uniqueness: true, :exclusion => { :in => "Pat" }
 	validates :password, presence: true
 	validates :password_confirmation, presence: true
 	
