806f18864604f4db0fdc7aeed5235afb3ea6b569
Richie <zirbsster@gmail.com>
2012-03-03 12:41:14 -0700

    updated author table to have photo column

--- project05/db/schema.rb
+++ project05/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120219175129) do
+ActiveRecord::Schema.define(:version => 20120303193954) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120219175129) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo"
   end
 
 end
