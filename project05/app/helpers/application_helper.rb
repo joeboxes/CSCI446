66eb7d85d06520b73ad558152c32006e00f90525
Richie <zirbsster@gmail.com>
2012-03-04 12:49:20 -0700

    editing code files for image icons

--- project05/app/helpers/application_helper.rb
+++ project05/app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+
+	def get_icon_mail
+		image_tag('icons/email.png')
+	end
+	def get_icon_show
+		image_tag('icons/magnifier.png')
+	end
+	def get_icon_edit
+		image_tag('icons/cog.png')
+	end
+	def get_icon_delete
+		image_tag('icons/cancel.png')
+	end
 end
