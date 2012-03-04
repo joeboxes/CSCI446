a46a65828d39c773590debe6b8879e70fbf00ff2
Richie <zirbsster@gmail.com>
2012-03-03 20:55:54 -0700

    got author validation to work

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,7 +1,21 @@
 class Author < ActiveRecord::Base
-	validates :username, presence: true, uniqueness: true, :exclusion => { :in => "Pat" }
+	validates :username, presence: true, uniqueness: true
+		validate :no_author_named_pat
+	validates :email, presence: true
 	validates :password, presence: true
 	validates :password_confirmation, presence: true
 	
 	has_secure_password
+
+	private
+		
+		def increment_count_on_update
+			self.update_count += 1 unless self.new_record?
+		end
+
+		def no_author_named_pat
+			if username.downcase.include? 'pat'
+				self.errors.add :username, "No authors named Pat"
+			end
+		end
 end
